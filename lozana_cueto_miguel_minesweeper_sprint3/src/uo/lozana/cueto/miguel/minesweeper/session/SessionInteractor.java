4503a2778067566c27b09508145fc590f2a0d219
MiguelLozana <miguellozanacueto@gmail.com>
2026-04-02 12:00:10 +0200

    docuemtnacuon de Sesuion Interactor

--- lozana_cueto_miguel_minesweeper_sprint3/src/uo/lozana/cueto/miguel/minesweeper/session/SessionInteractor.java
+++ lozana_cueto_miguel_minesweeper_sprint3/src/uo/lozana/cueto/miguel/minesweeper/session/SessionInteractor.java
@@ -6,15 +6,49 @@ import uo.lozana.cueto.miguel.minesweeper.ranking.GameRankingEntry;
 
 public interface SessionInteractor {
 	
+	/**
+	 * Ask the user the level/difficulty 
+	 * @return the response a a GameLevel
+	 */
 	GameLevel askGameLevel();
-	
+	/**
+	 * Ask the user the userName. It cannot be blank
+	 * @return the valid userName
+	 */
     String askUserName();
+    /**
+     * Ask the user to choose an option in the menu. 0 = exist; >0 valid menu options
+     * @return the number which represent the menu option 
+     */
 	int askNextOption();
+	/**
+	 * Ask the user to decide to register the score
+	 * @return true if decide to register, false otherwise
+	 */
 	boolean doYouWantToRegisterYourScore();
+	/**
+	 * Shows all the elements of the list provided
+	 * @param ranking the list to show
+	 */
 	void showRanking(List<GameRankingEntry> ranking);
+	/**
+	 * Shows only the user ranking form the list provided
+	 * @param ranking the list to show
+	 */
 	void showPersonalRanking(List<GameRankingEntry> ranking);
+	/**
+	 * Shoes the GoodBy message
+	 */
 	void showGoodBye();
+	/**
+	 * Shows the error message to the user
+	 * @param message to show the user
+	 */
 	void showErrorMessage(String message);
+	/**
+	 * Shows the fatal error message to the user, the program will not continue
+	 * @param message to show the user
+	 */
 	void showFatalErrorMessage(String message);
 
 }
